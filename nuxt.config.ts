db3ebf48d7b136d095769a4c6effb6648c385b38
AleLuppi <alelup@yahoo.it>
2025-08-19 23:44:59 +0200

    Create main css with tailwind
    Create main.css and auto-import with Nuxt.
    Enable tailwind and nuxt/ui css.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/test-utils'
-  ]
-})
+  css: ["~/assets/css/main.css"],
+
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui", "@nuxt/test-utils"],
+});
