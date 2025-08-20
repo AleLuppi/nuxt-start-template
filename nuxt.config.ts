87bbabbd86ff5e68fa5bc03505308b45eff17613
AleLuppi <alelup@yahoo.it>
2025-08-20 19:01:27 +0200

    Allow .env.local files to store secrets
    Distinguish between .env and .env.local files:
    - .env will be used to store generic sharable env vars (committed)
    - .env.local stores secrets (not committed)
    Nuxt only loads .env, thus explicit loading of secrets is required.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,21 @@
+// Load environment variables from multiple local .env files.
+// For production, env vars should be set on the server (no .env file will be loaded).
+if (process.env.NODE_ENV) {
+  const { config } = await import("dotenv");
+
+  // main ".env" gets auto loaded from nuxt
+  config({ path: ".env.local", override: true });
+
+  // NODE_ENV-specific .env files are intentionally not loaded here,
+  // since relying on them is generally considered an anti-pattern:
+  //     https://nodejs.org/en/learn/getting-started/nodejs-the-difference-between-development-and-production
+  // If you still want to enable them, refer to the following example:
+  //     if (process.env.NODE_ENV === "development") {
+  //       config({ path: ".env.dev", override: true });
+  //       config({ path: ".env.local.dev", override: true });
+  //     }
+}
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
